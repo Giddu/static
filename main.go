d5e5c108778693cf786432776569540424224e0d
Vishal Gidwani <vishal.0694@gmail.com>
2024-11-06 17:47:29 +0530

    added path argument

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	args := os.Args
+	if len(args) == 1 {
+		log.Fatal("Path argument missing")
+	}
+
+	path := args[1]
+	log.Printf("Serving path: %v\n", path)
+
+	fs := http.FileServer(http.Dir(path))
+	http.Handle("/", fs)
+
+	log.Println("Server Started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
