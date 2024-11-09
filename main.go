cbe23e6f14186958e5a67c1eba14f3750c3097ed
Vishal Gidwani <vishal.0694@gmail.com>
2024-11-09 15:25:51 +0530

    poc changes for supporting hmr

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"text/template"
 
 	"github.com/fsnotify/fsnotify"
+)
+
+var (
+	servingPath  string
+	reloadMutex  sync.Mutex
+	shouldReload bool
 )
 
 func main() {
@@ -19,13 +31,13 @@ func main() {
 		log.Fatal("Path argument missing")
 	}
 
-	path := args[0]
-	log.Printf("Serving path: %v\n", path)
+	servingPath = args[0]
+	log.Printf("Serving servingPath: %v\n", servingPath)
 
-	go watchPath(path)
+	go watchPath(servingPath)
 
-	fs := http.FileServer(http.Dir(path))
-	http.Handle("/", fs)
+	http.HandleFunc("/", fsHandler)
+	http.HandleFunc("/reload", reloadHandler)
 
 	log.Printf("Server Started on localhost:%d\n", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
@@ -34,7 +46,7 @@ func main() {
 	}
 }
 
-func watchPath(path string) {
+func watchPath(servingPath string) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -49,6 +61,9 @@ func watchPath(path string) {
 				}
 				if event.Has(fsnotify.Write) {
 					log.Println("modified file:", event.Name)
+					reloadMutex.Lock()
+					shouldReload = true
+					reloadMutex.Unlock()
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -59,9 +74,60 @@ func watchPath(path string) {
 		}
 	}()
 
-	err = watcher.Add(path)
+	err = watcher.Add(servingPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func fsHandler(w http.ResponseWriter, r *http.Request) {
+	requestPath := r.URL.Path[1:]
+	if requestPath == "" {
+		requestPath = "."
+	}
+
+	stat, err := fs.Stat(os.DirFS(servingPath), requestPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	filePath := filepath.Join(servingPath, requestPath)
+	if stat.IsDir() {
+		filePath = filepath.Join(servingPath, "index.html")
+	}
+
+	fmt.Println("filePath", filePath)
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	if strings.HasSuffix(filePath, ".html") {
+		reloadScript := `<script>
+            const es = new EventSource('/reload');
+            es.onmessage = () => window.location.reload();
+        </script>`
+		tmpl := template.Must(template.New("html").Parse(string(content) + reloadScript))
+		tmpl.Execute(w, nil)
+	} else {
+		w.Write(content)
+	}
+}
+
+func reloadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for {
+		reloadMutex.Lock()
+		if shouldReload {
+			fmt.Fprintf(w, "data: reload\n\n")
+			w.(http.Flusher).Flush()
+			shouldReload = false
+		}
+		reloadMutex.Unlock()
+	}
 }
